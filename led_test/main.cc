a8848576a092e0c15be0eb9b20b94b466b356f7e
javaclerk <chg5638@gmail.com>
2023-02-03 17:11:02 +0900

    asf

--- led_test/main.cc
+++ led_test/main.cc
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+extern int gpio_num, led_num;
+
 int main(){
 
     led led;
@@ -12,11 +14,9 @@ int main(){
     cout << "Please Choose What Led You Want to Control" << endl;
     cout << "1.RUN LED    2.5G LED    3.C-V2X LED    4.WAVE LED    5.GNSS LED    6.EXIT" << endl;
     cin >> gpio_num;
-    led.out;
     cout << "Select Your LED Status Insert ( 0 is off, 1 is on)" << endl;
     cin >> led_num;
-
-    led.led_status;
+    led.out;
 
 }
 
