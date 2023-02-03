ecb6f32bc5b03ba0fa86f8a35fc2eb7dfddddd0d
javaclerk <chg5638@gmail.com>
2023-02-03 15:42:27 +0900

    LED 구조 수정

--- led_test/func/ledfunc.h
+++ led_test/func/ledfunc.h
@@ -1,67 +1,73 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
-#define SYS_GPIO_DIR "/sys/class/gpio"
+#define SYS_GPIO_DIR ("/sys/class/gpio")
 
-class LED{
+using namespace std;
+
+int gpio_num, led_num;
+std::string s;
+
+
+class led{
     public:
-    std::ofstream out;
-    int led_status, gpio_status;
 
-    void Out();
-    void On();
-    void Off();
+    static void led_status();
+    static void out();
 };
 
-enum gpio{
-    gpio487 = 487,
-    gpio488,
-    gpio489,
-    gpio490,
-    gpio491,
+enum GPIO{
+    enum_gpio487 = 1,
+    enum_gpio488,
+    enum_gpio489,
+    enum_gpio490,
+    enum_gpio491
 };
 
-void LED::Out(){
-    out.open(SYS_GPIO_DIR);
-    if(out.is_open()){
-        std::cout << "Please Insert Led Status ( 0 is lightout, 1 is light on )" << std::endl;
-        std::cin >> led_status;
-        std::cout << "Please Insert Led Control" << std::endl;
-        std::cout << "1.Power_LED 2.5G_LED 3.C-V2X_LED 4.WAVE_LED 5.GNSS_LED" "6.EXIT"<< std::endl;
-        std::cin >> gpio_status;
-        if(gpio_status = 487){
-            out.open("gpio487/value", std::ios::ate);
-        }
-        else if(gpio_status = 488){
-            out.open("gpio488/value", std::ios::ate);
-        }
-        else if(gpio_status = 489){
-            out.open("gpio489/value", std::ios::ate);
-        }
-        else if(gpio_status = 490){
-            out.open("gpio490/value", std::ios::ate);
-        }
-        else if(gpio_status = 491){
-            out.open("gpio491/value", std::ios::ate);
-        }
-        else if(gpio_status = 488){
-            out.open("gpio488/value", std::ios::ate);
-        }
+void out(){
+
+    if(gpio_num = enum_gpio487){
+        std::ofstream ofs("/gpio487/value", std::ios::ate);
     }
-    else if(out.fail()){
+    else if(gpio_num = enum_gpio488){
+        std::ofstream ofs("/gpio488/value", std::ios::ate);
+    }
+    else if(gpio_num = enum_gpio489){
+        std::ofstream ofs("/gpio489/value", std::ios::ate);
+    }
+    else if(gpio_num = enum_gpio490){
+        std::ofstream ofs("/gpio490/value", std::ios::ate);
+    }
+    else if(gpio_num = enum_gpio491){
+        std::ofstream ofs("/gpio491/value", std::ios::ate);
+    }
+    else if(gpio_num = 6){
+        cout << "Exit The Program!!" << endl;
+    }
+    if(ofs.fail())
+    {
         std::cerr << "Error!" << std::endl;
     }
-    out.close();
+
+    ofs.close();
 }
 
-void LED::On(){
-    if(led_status = 1){
-        std::cout << "1" << std::endl;
-    }
-}
+void led_status(){
 
-void LED::Off(){
-    if(led_status = 0){
-        std::cout << "0" << std::endl;
+    if(ofs.is_open){
+        if(led_num = 1){
+            ofs << "1" << std::endl;
+        }
     }
+        else if(led_num = 0){
+            ofs << "0" << std::endl;
+    }
+
+    if(ofs.fail())
+    {
+        std::cerr << "Error!" << std::endl;
+    }
+
+    ofs.close();
 }
