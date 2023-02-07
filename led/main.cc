0e56fe7b32e14ff0ed4a2954c654f94172eba1ff
javaclerk <chg5638@gmail.com>
2023-02-07 10:36:11 +0900

    LED제어 구현중

--- led/main.cc
+++ led/main.cc
@@ -1,86 +1,23 @@
-#include<iostream>
-#include<fstream>
-#include<string>
-
-#include "leds/led.h"
+#include <iostream>
+#include <fstream>
+#include "func/ledfunc.h"
 
 using namespace std;
 
+int gpio_num, led_num;
+
 int main(){
 
-    fiveg_led fiveg_led;
-    gnss_led gnss_led;
-    pwr_led pwr_led;
-    v2x_led v2x_led;
-    wave_led wave_led;
+    led led;
 
-    int num,g_num;
-
-    std::cout << "LED control Program" << endl;
-    std::cout << "--------------------" << endl;
-    std::cout << "Please Choose control pannel" << endl;
-    std::cout << "--------------------" << endl;
-    std::cout << "1.Power_LED 2.5G_LED 3.C-V2X_LED 4.WAVE_LED 5.GNSS_LED" "6.EXIT"<< endl;
-    std::cin >> num;
+    cout << "This is LED Controller" << endl;
+    cout << "Please Choose What Led You Want to Control" << endl;
+    cout << "1.RUN LED    2.5G LED    3.C-V2X LED    4.WAVE LED    5.GNSS LED    6.EXIT" << endl;
+    cin >> gpio_num;
+    cout << "Select Your LED Status Insert ( 0 is off, 1 is on)" << endl;
+    cin >> led_num;
+    led.Out();
     
-
-    if(num == 1){
-        std::cout << "Power_LED" << endl;
-        std::cout << "Light Status (0 or 1)" << endl;
-        cin >> g_num;
-        if(g_num == 1){
-            pwr_led.On();
-        }
-        else{
-            pwr_led.Off();
-        }
-    }
-    else if(num == 2){
-        std::cout << "5G_LED" << endl;
-        std::cout << "Light Status (0 or 1)" << endl;
-        cin >> g_num;
-        if(g_num == 1){
-            fiveg_led.On();
-        }
-        else{
-            fiveg_led.Off();
-        }
-    }
-    else if(num == 3){
-        std::cout << "C-V2X_LED" << endl;
-        std::cout << "Light Status (0 or 1)" << endl;
-        cin >> g_num;
-        if(g_num == 1){
-            v2x_led.On();
-        }
-        else{
-            v2x_led.Off();
-        }
-    }
-    else if(num == 4){
-        std::cout << "WAVE_LED" << endl;
-        std::cout << "Light Status (0 or 1)" << endl;
-        cin >> g_num;
-        if(g_num == 1){
-            wave_led.On();
-        }
-        else{
-            wave_led.Off();
-        }
-    }
-    else if(num == 5){
-        std::cout << "GNSS_LED" << endl;
-        std::cout << "Light Status (0 or 1)" << endl;
-        cin >> g_num;
-        if(g_num == 1){
-            gnss_led.On();
-        }
-        else{
-            gnss_led.Off();
-        }
-    }
-    else{
-        std::cout << "Invalid Command !!!" << endl;
-    }
     return 0;
 }
+
--- led_test/main.cc
+++ led_test/main.cc
@@ -1,22 +1,17 @@
 #include <iostream>
 #include <fstream>
-#include "func/ledfunc.h"
-
-using namespace std;
-
-
-int gpio_num, led_num;
+#include <string>
 
 int main(){
+    std::ifstream in ("sys/class/gpio/gpio490/value");
+    std::string s;
 
-    led led;
+    if (in.is_open()) {
+        in >> s;
+        std::cout << "1" << std::endl;
+    } else {
+        std::cout << "파일을 찾을 수 없습니다!" << std::endl;
+    }
 
-    cout << "This is LED Controller" << endl;
-    cout << "Please Choose What Led You Want to Control" << endl;
-    cout << "1.RUN LED    2.5G LED    3.C-V2X LED    4.WAVE LED    5.GNSS LED    6.EXIT" << endl;
-    cin >> gpio_num;
-    cout << "Select Your LED Status Insert ( 0 is off, 1 is on)" << endl;
-    cin >> led_num;
-    led.Out();
+    return 0;
 }
-
