1e93e619c42ec8afb1493dc655ed3c38ad99ca7f
javaclerk <chg5638@gmail.com>
2023-02-08 10:09:05 +0900

    temp 간단 구현완성

--- temp_test/func/temp.h
+++ temp_test/func/temp.h
@@ -2,27 +2,53 @@
 #include <fstream>
 #include <string>
 
-int value;
-
 using namespace std;
+
+int value;
+extern int temp_sensor;
 
 class temperature{
     public:
     
+    void temp_logic();
     void temp_zone0();
     void temp_zone1();
 };
 
 void temp_zone0()
 {
-    ifstream ifs("/sys/class/thermal/thermal_zone0/temp");
+    ifstream in("/sys/class/thermal/thermal_zone0/temp");
 
-    ifs >> value;
+    if (in.is_open()){
+        in >> value;
+        std::cout << "temp_zone0 temperature is :: " << value << std::endl;
+    }
+    else {
+        std::cout << "Error!!!" << std::endl;
+    }
+    in.close();
+
 }
 
 void temp_zone1()
 {
-    ifstream ifs("/sys/class/thermal/thermal_zone1/temp");
+    ifstream in("/sys/class/thermal/thermal_zone1/temp");
 
-    ifs >> value;
+    if (in.is_open()){
+        in >> value;
+        std::cout << "temp_zone1 temperature is :: " << value << std::endl;
+    }
+    else {
+        std::cout << "Error!!!" << std::endl;
+    }
+    in.close();
+}
+
+void temp_logic(){
+    if(temp_sensor == 1){
+        temp_zone0();
+    }
+    else if(temp_sensor == 2){
+        temp_zone1();
+    }
 }
